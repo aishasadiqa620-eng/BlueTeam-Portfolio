0ff26b9a4552539654791cd0a13d8fc992379450
ja620-eng <ja620-eng@github.com>
2026-09-20 03:26:27 -0700

    safai - sirf 5 files rakhi

--- day16_soc.py
+++
@@ -1,9 +0,0 @@
-# Day 16 - soc log checker
-with open("BlueTeam-Portfolio/fake_log.txt","r") as file:
-    data = file.read()
-    print("---Hamari log file---")
-    print(data)
-    if "Sargodha" in data:
-        print("Alert: Sargodha wali analyst mil gye")
-        print("Check Complete!")
-        
