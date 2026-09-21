9e39ddc3a267232665a4cf1df8d878b03cfc300b
ja620-eng <ja620-eng@github.com>
2026-09-21 02:33:29 -0700

    practice

---
+++ day17.py
@@ -0,0 +1,22 @@
+print(6 + 2)      # kya aayega?
+print("6" + "2")  # kya aayega?
+print("Hello" + "World")
+print("Meri fails: " + "6")
+ip = "192.168.1.10"
+print(ip)
+print(type(ip))
+fails = 3
+print(fails > 5)
+fails = 8
+
+# Ye sirf batayega
+print(fails > 5)  # False
+
+# Ye action lega
+if fails > 5:
+    print("ALERT! Block karo!")
+else:
+    print("Safe hai, kuch nahi karna")
+
+
+
